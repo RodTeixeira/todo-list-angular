49ab99db2576165f7f18520ca3bcba41f592fc75
RodTeixeira <rodofikis@hotmail.com>
2024-05-17 12:15:15 -0300

    dodo angular

---
+++ src/app/services/todo-signals.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, signal } from '@angular/core';
+import { Todo } from '../models/model/todo.model';
+import { TodoKeyLocalStorage } from '../models/model/enum/todoKeyLocalStorage';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodoSignalsService {
+  public todosState = signal<Array<Todo>>([]);
+
+  updateTodos(todo: Todo) {
+    if (todo !== null || undefined) {
+      this.todosState.mutate((todos) => {
+        if (todos !== null) {
+          todos.push(
+            new Todo(todo.id, todo.title, todo.description, todo.done)
+          );
+        }
+      });
+      this.saveTodosInLocalStorage();
+    }
+  }
+
+  saveTodosInLocalStorage() {
+    const todos = JSON.stringify(this.todosState());
+    todos && localStorage.setItem(TodoKeyLocalStorage.TODO_LIST, todos);
+  }
+}
