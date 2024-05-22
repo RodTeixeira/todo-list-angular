8ce6008f860aa00d5da498d3ec6d3b25b7d46ec0
RodTeixeira <rodofikis@hotmail.com>
2024-05-22 19:21:16 -0300

    teste

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,94 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { ComponentFixture, TestBed, fakeAsync, tick } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+import { first } from "rxjs";
+import { TodoSignalsService } from "./services/todo-signals.service";
+import { BrowserAnimationsModule, NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { Todo } from "./models/model/todo.model";
+import { DebugElement } from "@angular/core";
+import { By } from "@angular/platform-browser";
 
-describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [AppComponent]
-  }));
+describe("AppComponent", () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let todoSignalsService: TodoSignalsService;
 
-  it('should create the app', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [AppComponent, BrowserAnimationsModule, NoopAnimationsModule],
+      providers: [TodoSignalsService],
+    });
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    todoSignalsService = TestBed.inject(TodoSignalsService);
+    fixture.detectChanges();
+  });
+
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'todo-list-16' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('todo-list-16');
+  //Teste d @input
+  it("should set @Input() property correctly", () => {
+    component.projectName = "Testing angular";
+    fixture.detectChanges();
+    expect(component.projectName).toEqual("Testing angular");
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('todo-list-16 app is running!');
+  // teste de @output() e @input()
+  it("should emit event with @output decorator correctly", () => {
+    component.projectName = "testing my angular aplication";
+    component.outputEvent.pipe(first()).subscribe({
+      next: (event) => {
+        expect(event).toEqual("testing my angular aplication");
+        component.handleEmitEvent();
+      },
+    });
   });
+
+  //teste de acionamento de serviço e de um signal
+  it("should create new todo correctly and call service method", () => {
+    jest.spyOn(todoSignalsService, "updateTodos");
+    const newTodo: Todo = {
+      id: 0,
+      title: "Testing",
+      description: "Test new todo",
+      done: true,
+    };
+
+    component.handleCreateTodo(newTodo);
+    fixture.detectChanges();
+    expect(todoSignalsService.updateTodos).toHaveBeenCalledWith(newTodo);
+    expect(component.todoSignal()).toEqual([newTodo]);
+  });
+
+  //Teste de um elemento do Dom
+  it("should not render paragraph in the DOM", () => {
+    const component: DebugElement = fixture.debugElement;
+    const element: HTMLElement = component.nativeElement;
+    const paragraph = element.querySelector("p");
+    expect(paragraph).toBeNull;
+  });
+
+  it("should render paragraph in the DOM", () => {
+    component.renderTestMessage = true;
+    fixture.detectChanges();
+    const componentDebugElement: DebugElement = fixture.debugElement;
+    const element = componentDebugElement.query(By.css("p"));
+    const paragraph: HTMLElement = element.nativeElement;
+    expect(paragraph.textContent).toEqual("Test your angular application");
+  });
+
+  it("should isDoned property to be false", () => {
+    component.hanDlerCheckIsDone();
+    expect(component.isDone).toBe(false);
+  });
+
+  it("should isDoned property to be true", fakeAsync(() => {
+    component.hanDlerCheckIsDone();
+    tick(200);
+    expect(component.isDone).toBe(true);
+  }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,42 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './components/header/header.component';
-import { TodoCardComponent } from './components/todo-card/todo-card.component';
+import { Todo } from "src/app/models/model/todo.model";
+import { Component, EventEmitter, Input, Output, WritableSignal } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HeaderComponent } from "./components/header/header.component";
+import { TodoCardComponent } from "./components/todo-card/todo-card.component";
+import { TodoSignalsService } from "./services/todo-signals.service";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [CommonModule, HeaderComponent, TodoCardComponent],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'todo-list-16';
+  @Input() projectName!: string;
+  @Output() outputEvent = new EventEmitter<string>();
+  title = "todo-list-16";
+
+  todoSignal!: WritableSignal<Todo[]>;
+  renderTestMessage = false;
+  isDone = false;
+
+  constructor(private todoSignalsService: TodoSignalsService) {}
+
+  handleEmitEvent() {
+    this.outputEvent.emit(this.projectName);
+  }
+
+  handleCreateTodo(todo: Todo) {
+    if (todo) {
+      this.todoSignalsService.updateTodos(todo);
+      this.todoSignal = this.todoSignalsService.todosState;
+    }
+  }
+
+  hanDlerCheckIsDone() {
+    setTimeout(() => {
+      this.isDone = true;
+    }, 200);
+  }
 }
