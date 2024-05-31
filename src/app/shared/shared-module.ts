dbdb75a46f8632953ad89cc3661c0c7c3bb39cad
RodTeixeira <rodofikis@hotmail.com>
2024-05-31 17:44:00 -0300

    refector pipe

--- src/app/shared/shared-module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from "@angular/core";
-import { DateFormatPipe } from "./utils/pipes/date-format.pipe";
-
-@NgModule({
-  declarations: [DateFormatPipe],
-  exports: [DateFormatPipe],
-})
-export class SharedModule {}
